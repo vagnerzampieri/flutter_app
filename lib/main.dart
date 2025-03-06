c921dbb36ffc46bb6d040d01e39bcd37f6d655ee
Vagner Zampieri <vagner.zampieri@nuuvem.com>
2025-03-06 12:55:57 -0300

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title: Text("My App Bar"),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
         body: Center(
           child: Container(
             height: 300,
