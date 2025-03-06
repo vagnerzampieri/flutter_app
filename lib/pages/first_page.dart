dd13d2695d67c22261c3568278559dc489e480ed
Vagner Zampieri <vagner.zampieri@nuuvem.com>
2025-03-06 14:43:48 -0300

    Make some navigations

---
+++ lib/pages/first_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/second_page.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Page')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Go to Second Page"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
